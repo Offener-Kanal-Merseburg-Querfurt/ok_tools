41d8d49dc03b3a1237e50ac39ffd361d1e9da5dc
schulztr <C.Schulz-Trieglaff@gmx.de>
2022-09-01 11:18:32 +0200

    import legacy projects

--- ok_tools/settings.py
+++ ok_tools/settings.py
@@ -270,7 +270,7 @@ LOGGING = {
         },
         'console': {
             'handlers': ['console'],
-            'level': 'CRITICAL',
+            'level': 'ERROR',
             'propagate': True,
         }
     },
