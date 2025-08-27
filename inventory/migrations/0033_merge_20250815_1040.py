d6ec6a9fadc5949b59cfc767ca6b030ad14a8ddf
nilo <dh@gocept.com>
2025-08-27 11:12:14 +0200

    fix: add missing inventory merge migration; dedup i18n .po (de,en); update .gitignore

---
+++ inventory/migrations/0033_merge_20250815_1040.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inventory', '0032_remove_inventoryitem_object_type_and_more'),
+    ]
+
+    operations = []
